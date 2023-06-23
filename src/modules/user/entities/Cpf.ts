eb576d0963e91265f0e3f9a42b991b549a9c4388
andersonnaz <anderson_1004@hotmail.com>
2023-06-22 23:48:25 -0300

    feat: :sparkles: add validate method in cpf entity

--- src/modules/user/entities/Cpf.ts
+++ src/modules/user/entities/Cpf.ts
@@ -1,3 +1,4 @@
+const REGEX_VALIDATE_CPF = /([0-9]{2}[\.]?[0-9]{3}[\.]?[0-9]{3}[\/]?[0-9]{4}[-]?[0-9]{2})|([0-9]{3}[\.]?[0-9]{3}[\.]?[0-9]{3}[-]?[0-9]{2})/;
 export class Cpf {
   private readonly name: string;
   private readonly number: string;
@@ -5,5 +6,15 @@ export class Cpf {
   constructor(name: string, number: string){
     this.name = name;
     this.number = number;
+  }
+
+  static validate(number: string): boolean {
+    if(!number){
+      throw new Error('invalid cpf!');
+    }
+    if(!REGEX_VALIDATE_CPF.test(number)){
+      throw new Error('invalid cpf!');
+    }
+    return true;
   }
 }
