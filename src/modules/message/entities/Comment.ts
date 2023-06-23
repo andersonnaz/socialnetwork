2c8f07d6b11acab18cf56d64fdf8d245ced23b46
andersonnaz <anderson_1004@hotmail.com>
2023-06-23 13:33:10 -0300

    feat: :sparkles: add generateId method in comment entity

--- src/modules/message/entities/Comment.ts
+++ src/modules/message/entities/Comment.ts
@@ -1,3 +1,4 @@
+import { v4 as uuid } from 'uuid';
 export class Comment {
   private readonly _id: string;
   private readonly _postId: string;
@@ -6,9 +7,14 @@ export class Comment {
   private _content: string;
 
   private constructor(postId: string, authorId: string, content: string){
+    this._id = this.generateId();
     this._postId = postId;
     this._authorId = authorId;
     this._createdAt = new Date;
     this._content = content;
+  }
+
+  private generateId(): string {
+    return uuid();
   }
 }
