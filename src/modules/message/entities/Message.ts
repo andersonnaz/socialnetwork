9f6313e8c41f0e585ee34a5ef47d7bcd37d7e57c
andersonnaz <anderson_1004@hotmail.com>
2023-06-23 14:40:57 -0300

    refactor: :recycle: refacted variables in message entity

--- src/modules/message/entities/Message.ts
+++ src/modules/message/entities/Message.ts
@@ -1,15 +1,15 @@
 import { v4 as uuid } from "uuid";
 export class Message {
   private readonly _id: string;
-  private readonly _from: string;
-  private readonly _to: string;
+  private readonly _sender: string;
+  private readonly _recipient: string;
   private readonly _content: string;
   private readonly _createdAt: Date;
 
-  private constructor(from: string, to: string, content: string){
+  private constructor(sender: string, recipient: string, content: string){
     this._id = this.generateId();
-    this._from = from;
-    this._to = to;
+    this._sender = sender;
+    this._recipient = recipient;
     this._content = content;
     this._createdAt = new Date();
   }
