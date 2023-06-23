21d9bee033a58e28d560ac83f18f9bbf53d9a7bb
andersonnaz <anderson_1004@hotmail.com>
2023-06-22 23:23:03 -0300

    feat: :sparkles: add validate method in Address entity

--- src/modules/user/entities/Address.ts
+++ src/modules/user/entities/Address.ts
@@ -8,4 +8,17 @@ export class Address {
     this.number = number;
     this.neighborhood = neighborhood;
   }
+
+  static validate(street: string, number: number, neighborhood: string): boolean {
+    if(!street){
+      throw new Error('invalid street!');
+    }
+    if(!number){
+      throw new Error('invalid number!');
+    }
+    if(!neighborhood){
+      throw new Error('invalid neighborhood!');
+    }
+    return true;
+  }
 }
