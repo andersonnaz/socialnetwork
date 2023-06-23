06a0fb3f0b9210af925c4220532184d5e7e2eaea
andersonnaz <anderson_1004@hotmail.com>
2023-06-23 00:46:36 -0300

    feat: :sparkles: add getters and setters methods in user entity

--- src/modules/user/entities/User.ts
+++ src/modules/user/entities/User.ts
@@ -4,20 +4,20 @@ import { Email } from "./Email";
 import { v4 as uuid } from 'uuid';
 
 export class User {
-  private readonly id: string;
-  private readonly cpf: Cpf;
-  private readonly email: Email;
-  private name: string;
-  private address: Address;
-  private password: string;
+  private readonly _id: string;
+  private readonly _cpf: Cpf;
+  private readonly _email: Email;
+  private _name: string;
+  private _address: Address;
+  private _password: string;
 
   private constructor(name: string, address: Address, cpf: Cpf, email: Email, password: string){
-    this.id = this.generateId();
-    this.name = name;
-    this.address = address;
-    this.cpf = cpf;
-    this.email = email;
-    this.password = password;
+    this._id = this.generateId();
+    this._name = name;
+    this._address = address;
+    this._cpf = cpf;
+    this._email = email;
+    this._password = password;
   }
 
   static create(name: string, address: Address, cpf: Cpf, email: Email, password: string): User {
@@ -37,5 +37,62 @@ export class User {
 
   private generateId(): string {
     return uuid();
+  }
+
+  get id(): string {
+    return this._id;
+  }
+
+  get cpf(): string {
+    return this._cpf.number;
+  }
+
+  get email(): string {
+    return this._email.email;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  get address(): string {
+    return `
+      Rua: ${this._address.street}, ${this._address.number}
+      Bairro: ${this._address.neighborhood}
+    `
+  }
+
+  get password(): string {
+    return this._password;
+  }
+
+  set name(newName: string) {
+    if(!newName){
+      throw new Error('invalid new Name!')
+    }
+    if(newName === this._name){
+      throw new Error('new name is equal!')
+    }
+    this._name = newName;
+  }
+
+  set address(newAddress: Address){
+    if(!newAddress.street){
+      throw new Error('invalid new street!')
+    }
+    if(!newAddress.number){
+      throw new Error('invalid new number!');
+    }
+    if(!newAddress.neighborhood){
+      throw new Error('invalid new neighborhood!');
+    }
+    this._address = newAddress;
+  }
+
+  set password(newPassword: string){
+    if(!newPassword){
+      throw new Error('invalid new Password!');
+    }
+    this._password = newPassword;
   }
 }
