5be1586e4ac055469fca6313e9656d722e7b2460
andersonnaz <anderson_1004@hotmail.com>
2023-06-22 23:46:42 -0300

    feat: :sparkles: add cpf entity

---
+++ src/modules/user/entities/Cpf.ts
@@ -0,0 +1,9 @@
+export class Cpf {
+  private readonly name: string;
+  private readonly number: string;
+
+  constructor(name: string, number: string){
+    this.name = name;
+    this.number = number;
+  }
+}
