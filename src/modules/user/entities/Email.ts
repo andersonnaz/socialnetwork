04388b6eea9ea96ce45f2ffb0cf0841c631d7d3f
andersonnaz <anderson_1004@hotmail.com>
2023-06-23 00:06:54 -0300

    feat: :sparkles: add validate method in email entity

--- src/modules/user/entities/Email.ts
+++ src/modules/user/entities/Email.ts
@@ -1,3 +1,4 @@
+const REGEX_VALIDATE_EMAIL = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
 export class Email {
   private readonly email: string;
   private readonly name: string;
@@ -5,5 +6,15 @@ export class Email {
   constructor(name: string, email: string){
     this.name = name;
     this.email = email;
+  }
+
+  static validate(email: string): boolean {
+    if(!email){
+      throw new Error('invalid email!');
+    }
+    if(REGEX_VALIDATE_EMAIL.test(email)){
+      throw new Error('invalid email!');
+    }
+    return true;
   }
 }
