31cbfd6df8dedda1e78f426c3d4aec7cd7d85a7a
andersonnaz <anderson_1004@hotmail.com>
2023-06-23 13:57:03 -0300

    feat: :sparkles: add generateId method in post entity

--- src/modules/post/entities/Post.ts
+++ src/modules/post/entities/Post.ts
@@ -1,4 +1,5 @@
 import { Comment } from "../../message/entities/Comment";
+import { v4 as uuid } from "uuid";
 export class Post {
   private readonly _id: string;
   private readonly _authorId: string;
@@ -8,11 +9,16 @@ export class Post {
   private _comments: Comment[];
 
   private constructor(authorId: string, title: string, content: string){
+    this._id = this.generateId();
     this._authorId = authorId;
     this._title = title;
     this._content = content;
     this._date = new Date();
     this._comments = [];
+  }
+
+  private generateId(): string {
+    return uuid();
   }
 
 }
