7685398c656935dde5c52fd96c1c2a6e505a92d6
andersonnaz <anderson_1004@hotmail.com>
2023-06-23 13:40:42 -0300

    feat: :sparkles: add validate method in comment entity

--- src/modules/message/entities/Comment.ts
+++ src/modules/message/entities/Comment.ts
@@ -10,8 +10,21 @@ export class Comment {
     this._id = this.generateId();
     this._postId = postId;
     this._authorId = authorId;
-    this._createdAt = new Date;
+    this._createdAt = new Date();
     this._content = content;
+  }
+
+  static validate(postId: string, authorId: string, content: string): boolean {
+    if(!postId){
+      throw new Error('post id invalid!');
+    }
+    if(!authorId){
+      throw new Error('author id invalid!');
+    }
+    if(!content){
+      throw new Error('content invalid!');
+    }
+    return true;
   }
 
   private generateId(): string {
