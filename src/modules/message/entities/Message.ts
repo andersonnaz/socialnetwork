fce13fcd64192c12f226f23d7b86e2216ab3675d
andersonnaz <anderson_1004@hotmail.com>
2023-06-23 14:34:26 -0300

    feat: :sparkles: add generateId method in message entity

--- src/modules/message/entities/Message.ts
+++ src/modules/message/entities/Message.ts
@@ -1,3 +1,4 @@
+import { v4 as uuid } from "uuid";
 export class Message {
   private readonly _id: string;
   private readonly _from: string;
@@ -6,9 +7,14 @@ export class Message {
   private readonly _createdAt: Date;
 
   private constructor(from: string, to: string, content: string){
+    this._id = this.generateId();
     this._from = from;
     this._to = to;
     this._content = content;
     this._createdAt = new Date();
+  }
+
+  private generateId(): string {
+    return uuid();
   }
 }
