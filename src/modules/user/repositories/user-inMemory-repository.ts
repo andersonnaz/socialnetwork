31544d741f28b569635d82399ca9cc01dad41049
andersonnaz <anderson_1004@hotmail.com>
2023-06-28 18:10:04 -0300

    feat: :sparkles: add user in memory repository

---
+++ src/modules/user/repositories/user-inMemory-repository.ts
@@ -0,0 +1,39 @@
+import { User } from "../entities/User";
+import { UserRepository } from "./user-repository";
+
+type Tuple = [user: string, friends: string[]];
+
+class UserInMemoryRepository implements UserRepository{
+  private _database: User[] = [];
+  private _friendship: Tuple[] = [];
+
+  save(param: User): void {
+
+  }
+
+  list(): User[] {
+
+  }
+
+  delete(id: string): void {
+
+  }
+
+  update(id: string, param: Partial<User>): User {
+
+  }
+
+  findOne(id: string): User {
+
+  }
+
+  createFriendship(): void {
+
+  }
+
+  deleteFriendship(): void {
+
+  }
+}
+
+export default new UserInMemoryRepository();
