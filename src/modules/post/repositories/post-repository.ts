91d16b16751bd2a815f80074403dc0aeb5ecf6b5
andersonnaz <anderson_1004@hotmail.com>
2023-06-28 20:18:32 -0300

    feat: :sparkles: add post repository interface

---
+++ src/modules/post/repositories/post-repository.ts
@@ -0,0 +1,20 @@
+import { Save } from "@shared/database/repository/Save";
+import { Post } from "../entities/Post";
+import { Delete } from "@shared/database/repository/Delete";
+import { List } from "@shared/database/repository/List";
+import { Update } from "@shared/database/repository/Update";
+import { FindOne } from "@shared/database/repository/FindOne";
+import { Comment } from "@modules/comment/entities/Comment";
+
+export interface PostRepository extends
+  Save<Post>,
+  Delete,
+  List<Post>,
+  Update<Post>,
+  FindOne<Post>{
+
+  listUserPosts(userId: string): Post[];
+  createComment(postId: string, authorId: string, content: string): void;
+  editComment(commentId: string, content: string): Comment;
+  deleteComment(commentId: string): void;
+}
