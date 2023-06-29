220f9ce5f14d770fd46cc6f09e5333eb90eebc10
andersonnaz <anderson_1004@hotmail.com>
2023-06-29 14:17:45 -0300

    fix: :bug: removed method update in message repository interface

--- src/modules/message/repositories/message-repository.ts
+++ src/modules/message/repositories/message-repository.ts
@@ -1,14 +1,11 @@
 import { Save } from "@shared/database/repository/Save";
 import { Message } from "../entities/Message";
 import { FindOne } from "@shared/database/repository/FindOne";
-import { Update } from "@shared/database/repository/Update";
 import { Delete } from "@shared/database/repository/Delete";
-import { List } from "@shared/database/repository/List";
 
 export interface MessageRepository extends
   Save<Message>,
   FindOne<Message>,
-  Update<Message>,
   Delete{
 
   listBySender(userId: string): Message[];
