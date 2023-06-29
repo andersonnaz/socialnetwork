bad29fa6bcae748498fed46dd6918797d467b920
andersonnaz <anderson_1004@hotmail.com>
2023-06-29 17:04:38 -0300

    feat: :sparkles: add post edit comment service

---
+++ src/modules/post/services/post-edit-comment-service.ts
@@ -0,0 +1,15 @@
+import { Comment } from "@modules/comment/entities/Comment";
+import { PostRepository } from "../repositories/post-repository";
+
+export class PostEditCommentService {
+  private readonly _postRepository: PostRepository;
+
+  constructor(postRepository: PostRepository){
+    this._postRepository = postRepository;
+  }
+
+  async execute(postId: string, commentId: string, content: string): Promise<Comment> {
+    const comment = await this._postRepository.editComment(postId, commentId, content);
+    return comment;
+  }
+}
