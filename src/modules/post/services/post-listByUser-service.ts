8686f006acc201063dec8c052fb3508acc37f948
andersonnaz <anderson_1004@hotmail.com>
2023-06-29 17:03:43 -0300

    feat: :sparkles: add post list by user service

---
+++ src/modules/post/services/post-listByUser-service.ts
@@ -0,0 +1,15 @@
+import { Post } from "../entities/Post";
+import { PostRepository } from "../repositories/post-repository";
+
+export class PostListByUserService {
+  private readonly _postRepository: PostRepository;
+
+  constructor(postRepository: PostRepository){
+    this._postRepository = postRepository;
+  }
+
+  async execute(userId: string): Promise<Post[]> {
+    const posts = await this._postRepository.listUserPosts(userId);
+    return posts;
+  }
+}
