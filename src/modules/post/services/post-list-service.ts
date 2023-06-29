4693b32800dfeef7b946056a9d8526696378bf6d
andersonnaz <anderson_1004@hotmail.com>
2023-06-29 17:03:02 -0300

    feat: :sparkles: add post list service

---
+++ src/modules/post/services/post-list-service.ts
@@ -0,0 +1,15 @@
+import { Post } from "../entities/Post";
+import { PostRepository } from "../repositories/post-repository";
+
+export class PostListService {
+  private readonly _postRepository: PostRepository;
+
+  constructor(postRepository: PostRepository){
+    this._postRepository = postRepository;
+  }
+
+  async execute(): Promise<Post[]> {
+    const posts = await this._postRepository.list();
+    return posts;
+  }
+}
