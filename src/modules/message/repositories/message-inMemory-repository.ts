d4c064a234fc96f12f3497dd040af9fa352d1448
andersonnaz <anderson_1004@hotmail.com>
2023-06-29 14:11:35 -0300

    feat: :sparkles: add message in memory repository

---
+++ src/modules/message/repositories/message-inMemory-repository.ts
@@ -0,0 +1,30 @@
+import { Message } from "../entities/Message";
+import { MessageRepository } from "./message-repository";
+
+export class MessageInMemoryRepository implements MessageRepository {
+  private _database: Message[] = [];
+
+  save(param: Message): void {
+
+  }
+
+  findOne(id: string): Message | undefined {
+
+  }
+
+  update(id: string, param: Partial<Message>): Message {
+
+  }
+
+  delete(id: string): void {
+
+  }
+
+  listBySender(userId: string): Message[] {
+
+  }
+
+  listByRecipier(userId: string): Message[] {
+
+  }
+}
