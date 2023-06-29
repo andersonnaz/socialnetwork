25c4574c4b5a0d05e6ff4f6abb2305d6dd5fef23
andersonnaz <anderson_1004@hotmail.com>
2023-06-29 15:52:09 -0300

    feat: :sparkles: add find user service

---
+++ src/modules/user/services/user-find-service.ts
@@ -0,0 +1,15 @@
+import { User } from "../entities/User";
+import { UserRepository } from "../repositories/user-repository";
+
+export class UserFindService {
+  private readonly _userRepository: UserRepository;
+
+  constructor(userRepository: UserRepository){
+    this._userRepository = userRepository;
+  }
+
+  async execute(id: string): Promise<User> {
+    const user = await this._userRepository.findOne(id);
+    return user as User;
+  }
+}
