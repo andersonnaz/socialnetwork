4c52a86814bae7e1195dbb59cc12afe6207173fb
andersonnaz <anderson_1004@hotmail.com>
2023-06-29 17:02:32 -0300

    feat: :sparkles: add post delete service

---
+++ src/modules/post/services/post-delete-service.ts
@@ -0,0 +1,13 @@
+import { PostRepository } from "../repositories/post-repository";
+
+export class PostDeleteService {
+  private readonly _postRepository: PostRepository;
+
+  constructor(postRepository: PostRepository){
+    this._postRepository = postRepository;
+  }
+
+  async execute(id: string): Promise<void> {
+    await this._postRepository.delete(id);
+  }
+}
