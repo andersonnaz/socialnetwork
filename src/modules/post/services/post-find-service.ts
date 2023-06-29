df3ebb993dd67435e1a6c23dd038edc7b94d15a9
andersonnaz <anderson_1004@hotmail.com>
2023-06-29 17:02:47 -0300

    feat: :sparkles: add post find service

---
+++ src/modules/post/services/post-find-service.ts
@@ -0,0 +1,15 @@
+import { Post } from "../entities/Post";
+import { PostRepository } from "../repositories/post-repository";
+
+export class PostFindService {
+  private readonly _postRepository: PostRepository;
+
+  constructor(postRepository: PostRepository){
+    this._postRepository = postRepository;
+  }
+
+  async execute(id: string): Promise<Post> {
+    const post = await this._postRepository.findOne(id);
+    return post as Post;
+  }
+}
