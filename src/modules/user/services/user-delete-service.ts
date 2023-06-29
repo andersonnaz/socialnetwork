843094e6cc56eeff6b80f2c595b6e9b6b3d244fa
andersonnaz <anderson_1004@hotmail.com>
2023-06-29 15:51:41 -0300

    feat: :sparkles: add delete user service

---
+++ src/modules/user/services/user-delete-service.ts
@@ -0,0 +1,13 @@
+import { UserRepository } from "../repositories/user-repository";
+
+export class UserDeleteService {
+  private readonly _userRepository: UserRepository;
+
+  constructor(userRepository: UserRepository){
+    this._userRepository = userRepository;
+  }
+
+  async execute(id: string): Promise<void> {
+    await this._userRepository.delete(id);
+  }
+}
