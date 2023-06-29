e942db68120788b843599eb3cbe265738c2f3f98
andersonnaz <anderson_1004@hotmail.com>
2023-06-29 17:04:04 -0300

    feat: :sparkles: add post add comment service

---
+++ src/modules/post/services/post-add-Comment-service.ts
@@ -0,0 +1,13 @@
+import { PostRepository } from "../repositories/post-repository";
+
+export class PostAddCommentService {
+  private readonly _postRepository: PostRepository;
+
+  constructor(postRepository: PostRepository){
+    this._postRepository = postRepository;
+  }
+
+  async execute(postId: string, authorId: string, content: string): Promise<void> {
+    await this._postRepository.createComment(postId, authorId, content);
+  }
+}
