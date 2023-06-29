470519ef2c5a609b90a042ba656a5a071da8d1dc
andersonnaz <anderson_1004@hotmail.com>
2023-06-29 17:03:21 -0300

    feat: :sparkles: add post update service

---
+++ src/modules/post/services/post-update-service.ts
@@ -0,0 +1,16 @@
+import { PostUpdateDTO } from "../dto/post-update-DTO";
+import { Post } from "../entities/Post";
+import { PostRepository } from "../repositories/post-repository";
+
+export class PostUpdateService {
+  private readonly _postRepository: PostRepository;
+
+  constructor(postRepository: PostRepository){
+    this._postRepository = postRepository;
+  }
+
+  async execute(id: string, postUpdate: PostUpdateDTO): Promise<Post> {
+    const post = await this._postRepository.update(id, postUpdate);
+    return post;
+  }
+}
