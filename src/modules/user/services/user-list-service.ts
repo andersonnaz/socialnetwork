9099522d39bf5d000cf03902f5bf24b951f660ac
andersonnaz <anderson_1004@hotmail.com>
2023-06-29 15:52:41 -0300

    feat: :sparkles: add list user service

---
+++ src/modules/user/services/user-list-service.ts
@@ -0,0 +1,15 @@
+import { User } from "../entities/User";
+import { UserRepository } from "../repositories/user-repository";
+
+export class UserListService {
+  private readonly _userRepository: UserRepository;
+
+  constructor(userRepository: UserRepository){
+    this._userRepository = userRepository;
+  }
+
+  async execute(): Promise<User[]> {
+    const users = await this._userRepository.list();
+    return users;
+  }
+}
