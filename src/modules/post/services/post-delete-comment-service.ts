3176a8efc54bd6060a0262078a9aa0e280177b8e
andersonnaz <anderson_1004@hotmail.com>
2023-06-29 17:04:23 -0300

    feat: :sparkles: add post delete comment service

---
+++ src/modules/post/services/post-delete-comment-service.ts
@@ -0,0 +1,13 @@
+import { PostRepository } from "../repositories/post-repository";
+
+export class PostDeleteCommentService {
+  private readonly _postRepository: PostRepository;
+
+  constructor(postRepository: PostRepository){
+    this._postRepository = postRepository;
+  }
+
+  async execute(postId: string, commentId: string): Promise<void> {
+    await this._postRepository.deleteComment(postId, commentId);
+  }
+}
