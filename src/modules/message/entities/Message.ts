5937d9cb6b45eb9755f2a75ea3be80de5b1a5767
andersonnaz <anderson_1004@hotmail.com>
2023-06-29 14:27:44 -0300

    refactor: :recycle: renamend variable recipier in message entity

--- src/modules/message/entities/Message.ts
+++ src/modules/message/entities/Message.ts
@@ -2,29 +2,29 @@ import { v4 as uuid } from "uuid";
 export class Message {
   private readonly _id: string;
   private readonly _sender: string;
-  private readonly _recipient: string;
+  private readonly _recipier: string;
   private readonly _content: string;
   private readonly _createdAt: Date;
 
-  private constructor(sender: string, recipient: string, content: string){
+  private constructor(sender: string, recipier: string, content: string){
     this._id = this.generateId();
     this._sender = sender;
-    this._recipient = recipient;
+    this._recipier = recipier;
     this._content = content;
     this._createdAt = new Date();
   }
 
-  static create(sender: string, recipient: string, content: string): Message {
-    this.validade(sender, recipient, content);
-    return new Message(sender, recipient, content);
+  static create(sender: string, recipier: string, content: string): Message {
+    this.validade(sender, recipier, content);
+    return new Message(sender, recipier, content);
   }
 
-  static validade(sender: string, recipient: string, content: string): boolean {
+  static validade(sender: string, recipier: string, content: string): boolean {
     if(!sender){
       throw new Error('sender user invalid!');
     }
-    if(!recipient){
-      throw new Error('recipient user invalid!');
+    if(!recipier){
+      throw new Error('recipier user invalid!');
     }
     if(!content){
       throw new Error('content invalid!');
@@ -44,8 +44,8 @@ export class Message {
     return this._sender;
   }
 
-  get recipient(): string {
-    return this._recipient;
+  get recipier(): string {
+    return this._recipier;
   }
 
   get content(): string {
