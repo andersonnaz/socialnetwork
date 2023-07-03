c1de616d83455d644feecff4abfd39737097b808
andersonnaz <anderson_1004@hotmail.com>
2023-07-03 10:36:08 -0300

    feat: :sparkles: add user create friendship controller

---
+++ src/modules/user/controllers/user-create-friendship/index.ts
@@ -0,0 +1,8 @@
+import userInMemoryRepository from "@modules/user/repositories/user-inMemory-repository";
+import { UserCreateFriendshipService } from "@modules/user/services";
+import { UserCreateFriendshipController } from "./user-create-friendship-controller";
+
+const userCreateFriendshipService = new UserCreateFriendshipService(userInMemoryRepository);
+const userCreateFriendshipController = new UserCreateFriendshipController(userCreateFriendshipService);
+
+export { userCreateFriendshipController };
---
+++ src/modules/user/controllers/user-create-friendship/user-create-friendship-controller.ts
@@ -0,0 +1,21 @@
+import { UserCreateFriendshipService } from "@modules/user/services";
+import { Request, Response } from "express";
+
+export class UserCreateFriendshipController {
+  private readonly _userCreateFriendshipService: UserCreateFriendshipService;
+
+  constructor(userCreateFriendshipService: UserCreateFriendshipService){
+    this._userCreateFriendshipService = userCreateFriendshipService;
+  }
+
+  execute = async (request: Request, response: Response): Promise<Response> => {
+    try {
+      const { userId } = request.params;
+      const { friendId } = request.body;
+      const result = await this._userCreateFriendshipService.execute(userId, friendId);
+      return response.status(200).json(result);
+    } catch (error) {
+      return response.status(404).json(error);
+    }
+  }
+}
