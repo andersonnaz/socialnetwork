6af092f2183a6dacff0a0db618644a3d6259250e
andersonnaz <anderson_1004@hotmail.com>
2023-07-03 08:37:10 -0300

    feat: :sparkles: add index file that contains all message services

---
+++ src/modules/message/services/index.ts
@@ -0,0 +1,5 @@
+export * from "./message-create-service";
+export * from "./message-delete-service";
+export * from "./message-find-service";
+export * from "./message-listByRecipier-service";
+export * from "./message-listBySender-service";
