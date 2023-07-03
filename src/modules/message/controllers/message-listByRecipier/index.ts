0289243367a6dd046219f6ca1c6943de874bcbf6
andersonnaz <anderson_1004@hotmail.com>
2023-07-03 14:17:59 -0300

    feat: :sparkles: add message list by recipier controller

---
+++ src/modules/message/controllers/message-listByRecipier/index.ts
@@ -0,0 +1,8 @@
+import messageInMemoryRepository from "@modules/message/repositories/message-inMemory-repository";
+import { MessageListByRecipierService } from "@modules/message/services";
+import { MessageListByRecipierController } from "./message-listByRecipier-controller";
+
+const messageListByRecipierService = new MessageListByRecipierService(messageInMemoryRepository);
+const messageListByRecipierController = new MessageListByRecipierController(messageListByRecipierService);
+
+export { messageListByRecipierController };
---
+++ src/modules/message/controllers/message-listByRecipier/message-listByRecipier-controller.ts
@@ -0,0 +1,20 @@
+import { MessageListByRecipierService } from "@modules/message/services";
+import { Request, Response, response } from "express";
+
+export class MessageListByRecipierController {
+  private readonly _messageListByRecipierService: MessageListByRecipierService;
+
+  constructor(messageListByRecipierService: MessageListByRecipierService){
+    this._messageListByRecipierService = messageListByRecipierService;
+  }
+
+  execute = async (request: Request, response: Response): Promise<Response> => {
+    try {
+      const { userId } = request.params;
+      const result = await this._messageListByRecipierService.execute(userId);
+      return response.status(200).json(result);
+    } catch (error) {
+      return response.status(404).json(error);
+    }
+  }
+}
