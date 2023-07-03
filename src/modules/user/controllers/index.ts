f7052f7de26b633a2934ce0eef67a548d2cedad6
andersonnaz <anderson_1004@hotmail.com>
2023-07-03 10:39:41 -0300

    feat: :sparkles: add index file that contains all user controllers

---
+++ src/modules/user/controllers/index.ts
@@ -0,0 +1,7 @@
+export * from "./user-create";
+export * from "./user-create-friendship";
+export * from "./user-delete";
+export * from "./user-delete-friendship";
+export * from "./user-find";
+export * from "./user-list";
+export * from "./user-update";
