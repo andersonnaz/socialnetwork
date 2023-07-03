f383e9707445ad881274da1975f0a386d4792f0c
andersonnaz <anderson_1004@hotmail.com>
2023-07-03 15:10:00 -0300

    feat: :sparkles: add post routes

---
+++ src/modules/post/routes/post-routes.ts
@@ -0,0 +1,26 @@
+import { Router } from "express";
+import {
+  postAddCommentController,
+  postCreateController,
+  postDeleteCommentController,
+  postDeleteController,
+  postEditCommentController,
+  postFindController,
+  postListByUserController,
+  postListController,
+  postUpdateController
+} from "../controllers";
+
+const postRouter = Router();
+
+postRouter.get('/', postListController.execute);
+postRouter.post('/', postCreateController.execute);
+postRouter.delete('/:id', postDeleteController.execute);
+postRouter.get('/:id', postFindController.execute);
+postRouter.put('/:id', postUpdateController.execute);
+postRouter.post('/comment/', postAddCommentController.execute);
+postRouter.delete('/comment/:postId', postDeleteCommentController.execute);
+postRouter.put('/comment/:postId', postEditCommentController.execute);
+postRouter.get('/user/:userId', postListByUserController.execute);
+
+export default postRouter;
