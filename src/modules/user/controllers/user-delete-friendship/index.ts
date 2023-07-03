2e7c333a6989d355daf3175763cdc41c21d9e8da
andersonnaz <anderson_1004@hotmail.com>
2023-07-03 10:38:03 -0300

    feat: :sparkles: add user delete friendship controller

---
+++ src/modules/user/controllers/user-delete-friendship/index.ts
@@ -0,0 +1,8 @@
+import userInMemoryRepository from "@modules/user/repositories/user-inMemory-repository";
+import { UserDeleteFriendshipService } from "@modules/user/services";
+import { UserDeleteFriendshipController } from "./user-delete-friendship-controller";
+
+const userDeleteFriendshipService = new UserDeleteFriendshipService(userInMemoryRepository);
+const userDeleteFriendshipController = new UserDeleteFriendshipController(userDeleteFriendshipService);
+
+export { userDeleteFriendshipController };
---
+++ src/modules/user/controllers/user-delete-friendship/user-delete-friendship-controller.ts
@@ -0,0 +1,21 @@
+import { UserDeleteFriendshipService } from "@modules/user/services";
+import { Request, Response } from "express";
+
+export class UserDeleteFriendshipController {
+  private readonly _userDeleteFriendshipService: UserDeleteFriendshipService;
+
+  constructor(userDeleteFriendshipService: UserDeleteFriendshipService){
+    this._userDeleteFriendshipService = userDeleteFriendshipService;
+  }
+
+  execute = async (request: Request, response: Response): Promise<Response> => {
+    try {
+      const { userId } = request.params;
+      const { friendId } = request.body;
+      const result = await this._userDeleteFriendshipService.execute(userId, friendId);
+      return response.status(200).json(result);
+    } catch (error) {
+      return response.status(404).json(error);
+    }
+  }
+}
