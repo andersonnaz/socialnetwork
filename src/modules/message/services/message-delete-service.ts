2fac78addc0345512c5a35b2000f08d964b55cfc
andersonnaz <anderson_1004@hotmail.com>
2023-07-03 08:34:27 -0300

    feat: :sparkles: add message delete service

---
+++ src/modules/message/services/message-delete-service.ts
@@ -0,0 +1,13 @@
+import { MessageRepository } from "../repositories/message-repository";
+
+export class MessageDeleteService {
+  private readonly _messageRepository: MessageRepository;
+
+  constructor(messageRepository: MessageRepository){
+    this._messageRepository = messageRepository;
+  }
+
+  async execute(id: string): Promise<void> {
+    await this._messageRepository.delete(id);
+  }
+}
