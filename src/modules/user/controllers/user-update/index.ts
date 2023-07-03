f2af3a70a2385a4356d658c66a319a01260b7cdd
andersonnaz <anderson_1004@hotmail.com>
2023-07-03 10:39:07 -0300

    feat: :sparkles: add user update controller

---
+++ src/modules/user/controllers/user-update/index.ts
@@ -0,0 +1,8 @@
+import userInMemoryRepository from "@modules/user/repositories/user-inMemory-repository";
+import { UserUpdateService } from "@modules/user/services";
+import { UserUpdateController } from "./user-update-controller";
+
+const userUpdateService = new UserUpdateService(userInMemoryRepository);
+const userUpdateController = new UserUpdateController(userUpdateService);
+
+export { userUpdateController };
---
+++ src/modules/user/controllers/user-update/user-update-controller.ts
@@ -0,0 +1,21 @@
+import { UserUpdateService } from "@modules/user/services";
+import { Request, Response } from "express";
+
+export class UserUpdateController {
+  private readonly _userUpdateService: UserUpdateService;
+
+  constructor(userUpdateService: UserUpdateService){
+    this._userUpdateService = userUpdateService;
+  }
+
+  execute = async (request: Request, response: Response): Promise<Response> => {
+    try {
+      const { id } = request.params;
+      const { name, password } = request.body;
+      const result = await this._userUpdateService.execute(id, {name, password});
+      return response.status(200).json(result);
+    } catch (error) {
+      return response.status(404).json(error);
+    }
+  }
+}
