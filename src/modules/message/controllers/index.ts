312700d190db2f8b2f43f75461a5b78ff3800c81
andersonnaz <anderson_1004@hotmail.com>
2023-07-03 14:21:11 -0300

    feat: :sparkles: add file that contains all message controllers

---
+++ src/modules/message/controllers/index.ts
@@ -0,0 +1,5 @@
+export * from "./message-create";
+export * from "./message-delete";
+export * from "./message-find";
+export * from "./message-listByRecipier";
+export * from "./message-listBySender";
