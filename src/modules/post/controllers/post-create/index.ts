1f127673678d5478efdc9fa1fc67375530d5eebf
andersonnaz <anderson_1004@hotmail.com>
2023-07-03 13:26:20 -0300

    feat: :sparkles: add post create controller

---
+++ src/modules/post/controllers/post-create/index.ts
@@ -0,0 +1,8 @@
+import postInMemoryRepository from "@modules/post/repositories/post-inMemory-repository";
+import { PostCreateService } from "@modules/post/services";
+import { PostCreateController } from "./post-create-controller";
+
+const postCreateService = new PostCreateService(postInMemoryRepository);
+const postCreateController = new PostCreateController(postCreateService);
+
+export { postCreateController };
