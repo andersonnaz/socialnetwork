3ce5aded86d5d5e600e26d20f6484affb319aab1
andersonnaz <anderson_1004@hotmail.com>
2023-07-04 14:20:11 -0300

    refactor: :recycle: refacted imports padronization

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import express from 'express';
-import router from "@shared/http/routes";
+import router from '@shared/http/routes';
 
-const app = express()
+const app = express();
 
 app.use(express.json());
 app.use(router);
